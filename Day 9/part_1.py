ef67cf5c80a45cc35b1509ee6f28313062ccfb88
SandbergM <marcus.a.sandberg@outlook.com>
2024-12-10 16:26:27 +0100

    Refactor puzzle processing logic in part_1.py and add Element class in part_2.py for improved data handling and clarity

--- Day 9/part_1.py
+++ Day 9/part_1.py
@@ -5,39 +5,50 @@ import datetime
 data = puzzle_data()
 
 start = datetime.datetime.now()
-visual = []
-ans = 0
 
-has_more = True
-idx = 0
+res = []
+jdx = 0
 
-while has_more:
-
-    if len(data):
-        free_space = data.pop(0)
-        visual.extend([str(idx) for x in range(free_space)])
-        idx += 1
+for idx, el in enumerate(data):
     
-    if len(data):
-        file_block = data.pop(0)
-        visual.extend(["." for x in range(file_block)])
+    if idx % 2 == 0:
+        for _ in range(el):
+            res.append(jdx)
+        jdx += 1
 
-    if len(data) == 0:
-        has_more = False
-ans = 0
-x = ""
+    if idx % 2 != 0:
+        for _ in range(el):
+            res.append(None)
+
 idx = 0
+ans = 0
 
-for idx, el in enumerate(visual):
-    print(f"{idx} : {len(visual)}")
-    if el == '.':
-        for j in range(len(visual) - 1, idx, -1):
-            if visual[j] != '.':
-                visual[idx], visual[j] = visual[j], visual[idx]
+while True:
+
+    if len(res) == 0:
+        break
+
+    e = res.pop(0)
+
+    if e is None:
+
+        while True:
+            
+            if len(res) == 0:
                 break
+            
+            e = res.pop()
+            
+            if e is not None:
+                ans += (idx * e)
+                break
+
+    else:
+        ans += (idx * e)
+
+    idx += 1
 
 end = datetime.datetime.now()
 
-print(x)
-print("Ans : ", sum([ idx * int(el) for idx, el in enumerate(visual) if el != '.'])) 
+print("Ans : ", ans) 
 print(f"Time taken: {(end - start).total_seconds() * 1000:.2f} ms")
---
+++ Day 9/part_2.py
@@ -0,0 +1,86 @@
+from data import puzzle_data, example_data
+import datetime
+
+
+data = puzzle_data()
+
+start = datetime.datetime.now()
+
+res = []
+jdx = 0
+ans = 0
+x = ""
+
+class Element:
+
+    def __init__(self, elements):
+        self.elements = elements
+        self.used_space = sum([ 1 if el is not None else 0 for el in self.elements])
+        self.free_space = sum([ 1 if el is None else 0 for el in self.elements])
+
+    def get_elements(self):
+        return self.__elements
+
+    def has_space(self, len_elements : int):
+        return self.free_space >= len_elements
+
+    def reset_space(self):
+        self.used_space = 0
+        self.free_space = len(self.elements)
+        self.elements = [None for _ in range(len(self.elements))]
+    
+    def fill_elements(self, elements):
+
+        idx = min([idx for idx in range(len(self.elements)) if self.elements[idx] is None])
+
+        for el in elements:
+            self.elements[idx] = el
+            idx += 1
+        
+        self.used_space = sum([ 1 if el is not None else 0 for el in self.elements])
+        self.free_space = sum([ 1 if el is None else 0 for el in self.elements])
+
+for idx, el in enumerate(data):
+    
+    if idx % 2 == 0 and el != 0:
+        res.append(Element([jdx for _ in range(el)]))
+        jdx += 1
+
+    if idx % 2 != 0 and el != 0:
+        res.append(Element([None for _ in range(el)]))
+
+
+idx = len(res) - 1
+
+while True:
+
+    current_element = res[idx]
+    
+    if idx < 0:
+        break
+
+    if current_element.used_space != 0:
+        for jdx in range(len(res)):
+            
+            if idx <= jdx:
+                break
+            
+            el = res[jdx]
+
+            if el.has_space(current_element.used_space):
+                el.fill_elements(current_element.elements)
+                current_element.reset_space()
+                break
+    
+    idx -= 1
+
+ans = []
+
+for e in res:
+    ans.extend(e.elements)
+
+ans = sum([idx * el for idx, el in enumerate(ans) if el is not None])
+end = datetime.datetime.now()
+
+print("Ans : ", ans) 
+print(f"Time taken: {(end - start).total_seconds() * 1000:.2f} ms")
